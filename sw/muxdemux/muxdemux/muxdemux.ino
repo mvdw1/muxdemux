3766c663dfb9fed4214ba738a921b57fe1e901d6
mvdw1 <mathijs@isthisdomainfree.com>
2024-07-02 21:07:12 +0200

    Simplify + fix bug when switching

--- sw/muxdemux/muxdemux/muxdemux.ino
+++ sw/muxdemux/muxdemux/muxdemux.ino
@@ -1,3 +1,8 @@
+/*
+ * SPDX-License-Identifier: GPL-3.0
+ */
+
+
 // (relevant) settings for ESP c3 super mini
 // Board: ESP32C3 Dev Module
 // USB CDC on boot: enabled
@@ -8,23 +13,13 @@
 
 // (relevant) settings for XIAO RP2040
 // board: Seed XIAO RP2040
-#define BOARD_XIAO_RP2040
+// CPU speed: 133 MHz
 
-#ifdef BOARD_ESP32_C3_SUPERMINI
-#define SEL_PIN 2
-#define OE_PIN 3
-#define BTN_PIN 4
-#define SEL_PWR_A_PIN 8
-#define SEL_PWR_B_PIN 7
-#endif
-#ifdef BOARD_XIAO_RP2040
-#define SEL_PIN D5
-#define OE_PIN D4
-#define BTN_PIN D8
-#define SEL_PWR_A_PIN D3
-#define SEL_PWR_B_PIN D2
-#endif
-#define VERSION "0.0.1" //TODO add e.g. git version 
+// Currently XIAO RP2040 is only tested
+
+#include "pinconfig.h"
+
+#define VERSION "0.0.3" // TODO add e.g. git version
 #define OK_CODE '1'
 #define ERR_CODE '2'
 
@@ -36,188 +31,304 @@ void pressedH();
 void pressedS();
 void pressedV();
 
+#define VERSION_0P3_UPPER 28
+#define VERSION_0P4_UPPER 84
+#define VERSION_0P5_UPPER 140
+
+enum BOARD_REV
+{
+  VERSION_0P3,
+  VERSION_0P4,
+  VERSION_0P5,
+  VERSION_UNKNOWN
+};
+
+int readVersionInfo()
+{
+  analogReadResolution(10);
+  long adcValue = analogRead(VERSION_PIN_DEFAULT);
+  Serial.println(adcValue);
+  if (adcValue < VERSION_0P3_UPPER)
+  {
+    return VERSION_0P3;
+  }
+  else
+  {
+    return VERSION_UNKNOWN;
+  }
+}
+const int *PINMAP;
+int setupPins()
+{
+  int version = readVersionInfo();
+  if (version == VERSION_0P3)
+  {
+    PINMAP = &PINMAP_0p3[0];
+    readVersionInfo();
+    if (PINMAP[BTN_PIN_INDEX] == BTN_PIN_0p3)
+    {
+      Serial.println("match");
+    }
+    else
+    {
+      Serial.println("mismatch");
+      Serial.println(PINMAP[BTN_PIN_INDEX]);
+      Serial.println(BTN_PIN_0p3);
+    }
+  }
+  else
+  {
+    while (1)
+    {
+      readVersionInfo();
+      Serial.println("Unsupported HW revision");
+      delay(1000);
+    }
+    // TODO go to error state
+  }
+
+  pinMode(PINMAP[BTN_PIN_INDEX], INPUT_PULLUP);
+  pinMode(PINMAP[SEL_PIN_INDEX], OUTPUT);
+  pinMode(PINMAP[OE_PIN_INDEX], OUTPUT);
+  pinMode(PINMAP[SEL_PWR_B_PIN_INDEX], OUTPUT);
+  pinMode(PINMAP[SEL_PWR_C_PIN_INDEX], OUTPUT);
+  pinMode(PINMAP[STATUS_LED_PIN_INDEX], OUTPUT);
+  return 0;
+}
+
 // Begin macro's for serial interface
 // Mapping macro with character, function to call, help text
-#define CHARMAP(F) \
-F('a', pressedA, "Activate output")\
-F('b', pressedB, "Connect outputs A <-> B")\
-F('c', pressedC, "Connect outputs A <-> C")\
-F('d', pressedD, "Deactivate output")\
-F('h', pressedH, "Print help")\
-F('s', pressedS, "Print current status")\
-F('v', pressedV, "Print version info")
+#define CHARMAP(F)                            \
+  F('a', pressedA, "Activate output")         \
+  F('b', pressedB, "Connect outputs A <-> B. Briefly disconnects upon execution")  \
+  F('c', pressedC, "Connect outputs A <-> C. Briefly disconnects upon execution") \
+  F('d', pressedD, "Deactivate output")       \
+  F('h', pressedH, "Print help")              \
+  F('s', pressedS, "Print current status")    \
+  F('v', pressedV, "Print version info")
 
 // Mapping from CHARMAP to case entries for switch
 #define CHARCASE(CHAR, FUNCTION, HELP) \
-case CHAR: FUNCTION(); break;
+  case CHAR:                           \
+    FUNCTION();                        \
+    break;
 // Mapping from CHARMAP to print help
-#define CHARHELP(CHAR, FUNCTION, HELP)\
-Serial.print(CHAR);Serial.print(": "); Serial.println(HELP);
+#define CHARHELP(CHAR, FUNCTION, HELP) \
+  Serial.print(CHAR);                  \
+  Serial.print(": ");                  \
+  Serial.println(HELP);
 // End macro's for serial interface
-
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200);
-  Serial.println("Starting muxdemux");
-  pinMode(BTN_PIN, INPUT_PULLUP);
-  pinMode(SEL_PIN, OUTPUT);
-  pinMode(OE_PIN, OUTPUT);
-  pinMode(SEL_PWR_A_PIN, OUTPUT);
-  pinMode(SEL_PWR_B_PIN, OUTPUT);
-  switchPorts(false, false);
-}
-
-bool readButton() {
-  return !digitalRead(BTN_PIN);
-}
-
-bool enableVoltageBridge(bool enableAB, bool enableAC) {
-  if (enableAB && enableAC) {
-    Serial.println("Enabling both not supported");
-  } else if (enableAB) {
-    digitalWrite(SEL_PWR_B_PIN, LOW);
-    digitalWrite(SEL_PWR_A_PIN, HIGH);
-  } else if (enableAC) {
-    digitalWrite(SEL_PWR_A_PIN, LOW);
-    digitalWrite(SEL_PWR_B_PIN, HIGH);
-  } else {
-    digitalWrite(SEL_PWR_A_PIN, LOW);
-    digitalWrite(SEL_PWR_B_PIN, LOW);
-  }
-  return true;
-}
-
-bool switchPorts(bool enabled, bool mode) {
-  if (!enabled) {
-    enableVoltageBridge(false, false);
-    digitalWrite(OE_PIN, HIGH);
-    digitalWrite(SEL_PIN, LOW);
-    return true;
-  }
-  if (mode) {
-    Serial.println("Connecting AB");
-    enableVoltageBridge(false, false);
-    delay(50);
-    digitalWrite(OE_PIN, LOW);
-    digitalWrite(SEL_PIN, LOW);
-    delay(50);
-    enableVoltageBridge(true, false);
-  } else {
-    Serial.println("Connecting AC");
-    enableVoltageBridge(false, false);
-    delay(50);
-    digitalWrite(OE_PIN, LOW);
-    digitalWrite(SEL_PIN, HIGH);
-    delay(50);
-    enableVoltageBridge(false, true);
-  }
-  return true;
-}
-bool enabled = true;
+#define AB false
+#define AC true
+#define OFF false
+#define ON true
+bool enabled = false;
 bool state = false;
 
-bool activateOutput() {
-Serial.println("activate");
-  enabled = true;
-  switchPorts(enabled, state);
-  return true;
+void setup()
+{
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+
+  // Default: off, a-b
+  enabled = OFF;
+  state = AB;
+  setupPins();
+  updateMode();
 }
-bool deactivateOutput() {
-  Serial.println("Deactivate");
-  enabled = false;
-  switchPorts(enabled, state);
-  return true;
+
+
+
+bool voltageABActive = false;
+bool voltageACActive = false;
+
+void updateMode()
+{
+  if(state == AB){
+    connectAC();
+  }else if(state == AC){
+    connectAB();
+  }else{
+    // Currently with state as type bool this is not possible,
+    // Place here for future change datatype
+    Serial.println("Invalid state!");
+  }
+  // update led
+  if(enabled == ON){
+    digitalWrite(PINMAP[STATUS_LED_PIN_INDEX], HIGH);
+  }else{
+    digitalWrite(PINMAP[STATUS_LED_PIN_INDEX], LOW);
+  }
 }
-bool selectAB() {
-  Serial.println("AB");
-  state = true;
-  switchPorts(enabled, state);
-  return true;
+
+void connectAB(){
+  // first power down
+  enableVoltageBridge(false, false);
+  digitalWrite(PINMAP[OE_PIN_INDEX], LOW);
+  delay(50);
+  digitalWrite(PINMAP[SEL_PIN_INDEX], LOW);
+  // apply power if needed
+  if(enabled == ON)
+  {
+    enableVoltageBridge(true, false);
+    digitalWrite(PINMAP[OE_PIN_INDEX], HIGH);
+  }
+  delay(50);
 }
-bool selectAC() {
-  Serial.println("AC");
-  state = false;
-  switchPorts(enabled, state);
+
+void connectAC(){
+  // first power down
+  enableVoltageBridge(false, false);
+  digitalWrite(PINMAP[OE_PIN_INDEX], LOW);
+  delay(50);
+  digitalWrite(PINMAP[SEL_PIN_INDEX], HIGH);
+  // apply power if needed
+  if(enabled == ON)
+  {
+    enableVoltageBridge(false, true);
+    digitalWrite(PINMAP[OE_PIN_INDEX], HIGH);
+  }
+  delay(50);
+}
+
+
+
+bool readButton()
+{
+  return !digitalRead(PINMAP[BTN_PIN_INDEX]);
+}
+
+
+bool enableVoltageBridge(bool enableAB, bool enableAC)
+{
+  if (enableAB && enableAC)
+  {
+    Serial.println("Enabling both not supported");
+  }
+  else if (enableAB)
+  {
+    digitalWrite(PINMAP[SEL_PWR_C_PIN_INDEX], LOW);
+    digitalWrite(PINMAP[SEL_PWR_B_PIN_INDEX], HIGH);
+    voltageABActive = true;
+    voltageACActive = false;
+  }
+  else if (enableAC)
+  {
+    digitalWrite(PINMAP[SEL_PWR_B_PIN_INDEX], LOW);
+    digitalWrite(PINMAP[SEL_PWR_C_PIN_INDEX], HIGH);
+    voltageABActive = false;
+    voltageACActive = true;
+  }
+  else
+  {
+    digitalWrite(PINMAP[SEL_PWR_B_PIN_INDEX], LOW);
+    digitalWrite(PINMAP[SEL_PWR_C_PIN_INDEX], LOW);
+    voltageABActive = false;
+    voltageACActive = false;
+  }
   return true;
 }
 
 
-void pressedA() {
-  activateOutput();
+
+
+
+void pressedA()
+{
+  enabled = ON;
+  updateMode();
   Serial.println(OK_CODE);
 }
 
-
-void pressedD() {
-  deactivateOutput();
+void pressedD()
+{
+  enabled = OFF;
+  updateMode();
   Serial.println(OK_CODE);
 }
 
-void pressedB() {
-  selectAB();
-  Serial.println(OK_CODE);
+void pressedB()
+{
+  // Switch to AB
+  state = AB;
+  updateMode();
 }
 
+void pressedC()
+{
 
-void pressedC() {
-  selectAC();
-  Serial.println(OK_CODE);
+  // Switch to AC
+  state = AC;
+  updateMode();
 }
 
-
-void pressedH() {
+void pressedH()
+{
   // Print help
   Serial.println("Command summary:");
   CHARMAP(CHARHELP)
-  
+
   Serial.println(OK_CODE);
 }
-void pressedS() {
+void pressedS()
+{
   Serial.println("Current state:");
-  if(enabled){
+  if (enabled)
+  {
     Serial.println("Output: enabled");
-  }else{
+  }
+  else
+  {
     Serial.println("Output: disabled");
   }
-  if(state){
+  if (state)
+  {
     Serial.println("Switch: AB");
-  }else{
+  }
+  else
+  {
     Serial.println("Switch: AC");
   }
-  
+
   Serial.println(OK_CODE);
 }
 
-void pressedV(){
+void pressedV()
+{
   Serial.println(VERSION);
   Serial.println(OK_CODE);
 }
 
-void checkSerialInput() {
-  //static String inputString = "";  // String to hold incoming serial data
+void checkSerialInput()
+{
+  // static String inputString = "";  // String to hold incoming serial data
   static bool newData = false;
   static char inChar = 0;
-  while (Serial.available() > 0) {
+  while (Serial.available() > 0)
+  {
     char newChar = (char)Serial.read();
 
     // Check for new line character
-    if (newChar == '\n') {
+    if (newChar == '\n')
+    {
       newData = true;
-    } else {
-      inChar = newChar;  // Read a character from serial
+    }
+    else
+    {
+      inChar = newChar; // Read a character from serial
     }
   }
 
-
   // If a complete newline is received, process the input
-  if (newData) {
+  if (newData)
+  {
     // Reset the variables for the next iteration
 
-
-    switch (inChar) {
-        CHARMAP(CHARCASE)
-        default:
-            Serial.println(ERR_CODE);
+    switch (inChar)
+    {
+      CHARMAP(CHARCASE)
+    default:
+      Serial.println(ERR_CODE);
     }
 
     inChar = 0;
@@ -228,65 +339,65 @@ void checkSerialInput() {
 bool currentState = false;
 bool lastButtonState = HIGH;
 bool buttonState;
-bool pressed = false;  // To track if the button was pressed
+bool pressed = false; // To track if the button was pressed
 
-unsigned long debounceDelay = 50;  // Debounce time in milliseconds
+unsigned long debounceDelay = 50; // Debounce time in milliseconds
 unsigned long lastDebounceTime = 0;
 
-int ssstate  = 0;
-void loop() {
-//  ssstate ++;
-//if(ssstate > 200){
-//digitalWrite(SEL_PIN, HIGH);
-//
-//}
-//else{
-//  digitalWrite(SEL_PIN,LOW);
-//  
-//}
-//if(ssstate>400) ssstate = 0;
-  // put your main code here, to run repeatedly:
-  delay(10);
-  //if (readButton()) {
-  //  Serial.println("btn press");
-  //}
-  checkSerialInput();
-
-
-  int reading = digitalRead(BTN_PIN);
+bool debouncedBtnRead()
+{
+  int reading = digitalRead(PINMAP[BTN_PIN_INDEX]);
 
   // Check for button state change with debouncing
-  if (reading != lastButtonState) {
+  if (reading != lastButtonState)
+  {
     lastDebounceTime = millis();
   }
 
-  if ((millis() - lastDebounceTime) > debounceDelay) {
+  if ((millis() - lastDebounceTime) > debounceDelay)
+  {
     // If the button state has been stable for the debounce delay
-    if (reading != buttonState) {
+    if (reading != buttonState)
+    {
       buttonState = reading;
 
       // If the button is pressed (LOW state due to INPUT_PULLUP)
-      if (buttonState == LOW) {
-        if (!pressed) {
-          if (state) {
-            enabled = true;
+      if (buttonState == LOW)
+      {
+        if (!pressed)
+        {
+          if (state == AB)
+          {
+            enabled = ON;
             pressedC();
-          } else {
-            enabled = true;
+          }
+          else
+          {
+            enabled = ON;
             pressedB();
           }
           pressed = true;
           currentState = !currentState;
         }
-      } else {
-        // Toggle the output state
-        //digitalWrite(OUTPUT_PIN, currentState ? HIGH : LOW);
-        pressed = false;  // Reset the pressed state
+      }
+      else
+      {
+        pressed = false; // Reset the pressed state
       }
     }
   }
 
   lastButtonState = reading;
+  return pressed;
 }
 
 
+
+
+void loop()
+{
+
+  delay(10);
+  debouncedBtnRead();
+  checkSerialInput();
+}
